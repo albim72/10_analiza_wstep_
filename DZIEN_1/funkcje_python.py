25cb35112ec57c65d1b2dc5996d5fbb37f3f1048
albim72 <marcin.albiniak72@gmail.com>
2024-10-23 13:09:40 +0200

    Create funkcje_python.py

---
+++ DZIEN_1/funkcje_python.py
@@ -0,0 +1,15 @@
+#przykład 0
+def przykladowa():
+    return 567
+
+print(przykladowa())
+
+def brak():
+    print("to jest funkcja która tylko wyświetla komunikat!")
+
+brak()
+
+def policz(a,b):
+    return a+(b-2)**2
+
+print(policz(3,6))
