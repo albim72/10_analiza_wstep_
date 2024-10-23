9c1f321cd2da2e9aa70207aea48ff694f3f2b806
albim72 <marcin.albiniak72@gmail.com>
2024-10-23 15:08:38 +0200

    Create main.py

---
+++ DZIEN_1/moje_moduly/main.py
@@ -0,0 +1,18 @@
+#import modułów wbudodwanych w interpreter pythona
+import math as mh
+#importy bibliotek zewnętrznych
+import numpy as np
+#importy bibliotek własnych
+
+
+#wynik działania math
+wynik = mh.sqrt(15)
+
+print(f"wynik: {wynik:.3f}")
+
+wynik2 = mh.pi
+print(f"wartośc pi: {wynik2}")
+
+#wynik działania numpy
+tablica = np.array([4,7,24,7,24])
+print(tablica)
