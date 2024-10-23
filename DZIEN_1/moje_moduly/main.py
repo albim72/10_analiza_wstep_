c6763b4975c44fa3e96b76b9ae42f0b28d91c4d1
albim72 <marcin.albiniak72@gmail.com>
2024-10-23 15:19:06 +0200

    Update main.py

--- DZIEN_1/moje_moduly/main.py
+++ DZIEN_1/moje_moduly/main.py
@@ -3,7 +3,8 @@ import math as mh
 #importy bibliotek zewnętrznych
 import numpy as np
 #importy bibliotek własnych
-
+import specjalny as sp
+from ekstraklasa.kolory import Kolory
 
 #wynik działania math
 wynik = mh.sqrt(15)
@@ -16,3 +17,15 @@ print(f"wartośc pi: {wynik2}")
 #wynik działania numpy
 tablica = np.array([4,7,24,7,24])
 print(tablica)
+
+#wynik działania sp
+print(sp.kolor)
+print(sp.LICZBA_SZTUK)
+print(sp.filia)
+
+print(sp.powitanie("Karol"))
+print(sp.policz(4,8))
+
+#wynik celowego importu Kolory
+kl = Kolory("zielony",111)
+print(kl.jakikod())
